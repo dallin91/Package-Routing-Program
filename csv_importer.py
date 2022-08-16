266d92731cd4ed233ccad721ffecaa009bb63099
Dallin Reeves <dallinreeves@gmail.com>
2022-08-15 19:00:52 -0500

    ability to read csv

--- csv_importer.py
+++ csv_importer.py
@@ -1,6 +1,32 @@
 import csv
 
+import csvfile as csvfile
+import value as value
+
 from hash_map import HashMap
 
 with open("WGUPS Package File.csv", "wt") as fp:
-    writer = csv.writer(fp, delimiter=",")
+    read_csv = csv.reader(csvfile, delimiter=',')
+    hash_map = HashMap()
+
+    for row in read_csv:
+        id = row[0]
+        address = row[1]
+        city = row[2]
+        state = row[3]
+        zip = row[4]
+        deadline = row[5]
+        weight = row[6]
+        note = row[7]
+        start = ''
+        location = ''
+        delivery_status = ''
+
+        value = [id, address, city, state, zip, deadline, weight, note,
+                 start, location, delivery_status]
+
+    hash_map.insert(id, value)
+
+
+def get_hash_map():
+    return hash_map
