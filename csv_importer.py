096eb5ec1fcfac8e449baa0f04c05ebbd6d7bc47
Dallin Reeves <dallinreeves@gmail.com>
2022-08-17 17:09:13 -0500

    working on csv hash functionality

--- csv_importer.py
+++ csv_importer.py
@@ -1,31 +1,54 @@
 import csv
 
-import value as value
-
 from hash_map import HashMap
 
-with open("WGUPS Package File.csv") as csvfile:
-    read_csv = csv.reader(csvfile)
-    hash_map = HashMap()
 
-    for row in read_csv:
-        id = row[0]
-        address = row[1]
-        city = row[2]
-        state = row[3]
-        zip = row[4]
-        deadline = row[5]
-        weight = row[6]
-        note = row[7]
-        start = ''
-        location = ''
-        delivery_status = ''
+class Package:
+    def __init__(self, id, address, city, state, zip, deadline, weight, note):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
+        self.weight = weight
+        self.note = note
 
-        value = [id, address, city, state, zip, deadline, weight, note,
-                 start, location, delivery_status]
-
-    hash_map.insert(id, value)
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s" % (self.id, self.address, self.city, self.state,
+                                                   self.zip, self.deadline, self.weight, self.note)
 
 
-def get_hash_map():
-    return hash_map
+packageHash = HashMap()
+
+def loadPackageData(filename):
+    with open(filename) as packageFile:
+        packageData = csv.reader(packageFile, delimiter=',')
+        next(packageData)
+        for package in packageData:
+            pId = int(package[0])
+            pAddress = package[1]
+            pCity = package[2]
+            pState = package[3]
+            pZip = package[4]
+            pDeadline = package[5]
+            pWeight = package[6]
+            pNote = package[7]
+            pStart = ''
+            pLocation = ''
+            pStatus = ''
+
+            package = Package(pId, pAddress, pCity, pState, pZip, pDeadline, pWeight,
+                              pNote, pStart, pLocation, pStatus)
+
+            packageHash.insert(pId, package)
+
+loadPackageData('WGUPS Package File.csv')
+
+
+
+
+
+def getPackages():
+    for i in range (len(packageHash)+1):
+        print("Key: {} and Package: {}".format(i+1, packageHash.search(i+1)))
