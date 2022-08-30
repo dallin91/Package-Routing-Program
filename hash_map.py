e36ec8c1539cfe198f717a1b0afc8ffd379e3801
Dallin Reeves <dallinreeves@gmail.com>
2022-08-30 16:51:35 -0500

    adjusting hash map

--- hash_map.py
+++ hash_map.py
@@ -7,7 +7,7 @@ class HashMap:
             self.table.append([])
 
     def insert(self, key, item):
-        bucket = hash(item) % len(self.table)
+        bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
 
         for kv in bucket_list:
@@ -19,6 +19,9 @@ class HashMap:
         bucket_list.append(key_value)
         return True
 
+    def __setitem__(self, key, value):
+        return self.insert(key, value)
+
     def search(self, key):
         bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
@@ -27,6 +30,9 @@ class HashMap:
             if key_value[0] == key:
                 return key_value[1]
         return None
+
+    def __getitem__(self, item):
+        return self.search(item)
 
     def remove(self, key):
         bucket = hash(key) % len(self.table)
--- main.py
+++ main.py
@@ -6,30 +6,29 @@ from csv_importer import loadPackageData
 from hash_map import HashMap
 
 
-class Main:
-    print("\n----------WGUPS Routing Program----------\n")
+print("\n----------WGUPS Routing Program----------\n")
 
-    user_input = input("UPDATE THIS LATER\n"
-                       "Type 'quit' if you would like to quit\n"
-                       "Type 'search' to look up a package\n"
-                       "Type 'hash' if you would like to see hash\n")
+user_input = input("UPDATE THIS LATER\n"
+                   "Type 'quit' if you would like to quit\n"
+                   "Type 'search' to look up a package\n"
+                   "Type 'hash' if you would like to see hash\n")
 
-    while user_input.lower() != 'quit':
-        loadPackageData('WGUPS Package File.csv')
-        packages = getPackages()
-        if user_input == 'hash':
-            print(type(packages))
-            print(len(packages.table))
-            break
-        elif user_input == 'search':
-            search_input = input("Enter the ID, delivery address, deadline, city, zip code, "
-                                 "package weight, or delivery status")
-            try:
-                package = packages.search(search_input)
-                print(package)
-            except ValueError:
-                print("Invalid input")
-                exit()
+while user_input.lower() != 'quit':
+    loadPackageData('WGUPS Package File.csv')
+    packages = getPackages()
+    if user_input == 'hash':
+        print(type(packages))
+        print(len(packages.table))
+        break
+    elif user_input == 'search':
+        search_input = input("Enter the ID, delivery address, deadline, city, zip code, "
+                             "package weight, or delivery status")
+        try:
+            package = packages.search(search_input)
+            print(package)
+        except ValueError:
+            print("Invalid input")
+            exit()
 
 
 
