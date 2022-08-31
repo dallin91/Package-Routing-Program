55fc6f7d64fa9ab95732556950d5c778959f3699
Dallin Reeves <dallinreeves@gmail.com>
2022-08-31 17:00:44 -0500

    initial commit

---
+++ trucks.py
@@ -0,0 +1,13 @@
+class Truck:
+    def __init__(self, capacity, speed, depart_time, packages, miles, address, loaded):
+        self.capacity = capacity
+        self.speed = speed
+        self.depart_time = depart_time
+        self.packages = packages
+        self.miles = miles
+        self.address = address
+        self.loaded = loaded
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.capacity, self.speed, self.depart_time, self.packages,
+                                               self.miles, self.address, self.loaded)
