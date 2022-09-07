fc651f1a9bb72bb29fb324cda004b3bffbb8c843
Dallin Reeves <dallinreeves@gmail.com>
2022-09-07 16:26:05 -0500

    tidied up + added comment

--- distances.py
+++ distances.py
@@ -2,13 +2,10 @@ from csv_importer import loadDistanceData
 
 distances = loadDistanceData('Distances.csv')
 
-
+# Method to find distance between two addresses
 def findDistance(x, y):
     distance = distances[x][y]
     if distance == '':
         distance = distances[y][x]
 
     return float(distance)
-
-
-print(distances)
